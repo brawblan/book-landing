c7687b153fb477f8a628afc5efe32498daf777d4
Andrey Codez <andreycodez@bixtrix.com>
2021-02-18 09:18:57 +1000

    FIX (REDUNDANT layaour elemes): Redundant br tags deleted

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -11,14 +11,11 @@ const Subscribe = () => {
           <input className='input' type="text" name='firstName' placeholder='First Name' />
           <div className={styles.nameIsTyped}>&#10003;</div>
         </div>
-          <br/>
-        <label htmlFor="">E-Mail</label> 
-          <br/>
+        <label htmlFor="">E-Mail</label>
         <div className={styles.checkMark}>
           <input type="email" placeholder='Your e-mail here' /> 
           <div className={styles.emailIsTyped}>&#10003;</div>
         </div>
-          <br/>
         <button type="email">SUBSCRIBE</button>
       </form>
     </div>
