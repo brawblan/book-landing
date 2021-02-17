5921d80ae19613957878fdd5503f42b8247eb86f
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-02-17 15:35:09 -0600

    api key attempt 3

--- pages/api/subscribeAPI.js
+++ pages/api/subscribeAPI.js
@@ -9,19 +9,19 @@ export default async (req, res) => {
     const FORM_ID = process.env.CONVERTKIT_FORM_ID;
     const API_KEY = process.env.CONVERTKIT_API_KEY;
     const API_URL = process.env.CONVERTKIT_API_URL;
-
+    
     const data = { api_key: API_KEY, email };
-
+    
     const response = await fetch(`${API_URL}forms/${FORM_ID}/subscribe`, {
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" },
       method: "POST",
     });
-
+    
     if (response.status >= 400) {
       return res
-        .status(400)
-        .json({ error: "There was an error subscribing to the list." });
+      .status(400)
+      .json({ error: "There was an error subscribing to the list." });
     }
 
     return res.status(201).json({ error: "" });
