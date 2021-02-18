427d28bbcdcf48cc9a28c9489ee0938ecc3b2cd0
Andrey Codez <andreycodez@bixtrix.com>
2021-02-19 02:52:51 +1000

    FIX (CHecked form field): A green circle appearing on checkedTrue

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -4,16 +4,18 @@ import styles from '../styles/Subscribe.module.css'
 const Subscribe = () => {
   return (
     <div className={styles.card}>
+      <h2>Subscribe here</h2>
+      <p>to get updates and free books</p>
       <form action="">
         <label htmlFor="">FIRST NAME</label>
         <div className={styles.checkMark}>
           <input className='input' type="text" name='firstName' placeholder='First Name' />
-          {/*<div className={styles.nameIsTyped}>&#10003;</div>*/}
+          <div className={styles.checkedTrueActive}>&#10003;</div>
         </div>
         <label htmlFor="">E-MAIL</label>
         <div className={styles.checkMark}>
           <input type="email" placeholder='Your e-mail here' /> 
-          {/*<div className={styles.emailIsTyped}>&#10003;</div>*/}
+          <div className={styles.checkedTrue}>&#10003;</div>
         </div>
         <button type="email">SUBSCRIBE</button>
       </form>
