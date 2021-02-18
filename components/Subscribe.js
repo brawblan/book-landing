72d83b715092c4c314bf96d1b1ca434d60d7858e
Andrey Codez <andreycodez@bixtrix.com>
2021-02-19 01:41:39 +1000

    FIX (Success form label): Commented a div for a succeess form label for a while

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -5,16 +5,15 @@ const Subscribe = () => {
   return (
     <div className={styles.card}>
       <form action="">
-        <label htmlFor="">FIRST NAME</label> 
-          <br/>
+        <label htmlFor="">FIRST NAME</label>
         <div className={styles.checkMark}>
           <input className='input' type="text" name='firstName' placeholder='First Name' />
-          <div className={styles.nameIsTyped}>&#10003;</div>
+          {/*<div className={styles.nameIsTyped}>&#10003;</div>*/}
         </div>
-        <label htmlFor="">E-Mail</label>
+        <label htmlFor="">E-MAIL</label>
         <div className={styles.checkMark}>
           <input type="email" placeholder='Your e-mail here' /> 
-          <div className={styles.emailIsTyped}>&#10003;</div>
+          {/*<div className={styles.emailIsTyped}>&#10003;</div>*/}
         </div>
         <button type="email">SUBSCRIBE</button>
       </form>
